21e44924044085312abd57e2fe52e78c46d26ea1
Joonnykk <jonnykk.web@gmail.com>
2023-09-05 19:11:25 +0300

    add franchize page

---
+++ avatargames/franchise/static/franchise/js/franchise.js
@@ -0,0 +1,24 @@
+import { firstSceneAnimation } from "./top/main.js";
+
+import '../css/style.css'
+
+window.addEventListener("DOMContentLoaded", () => {
+	firstSceneAnimation();
+//	fifteenthSceneAnimation();
+//	thirdSceneAnimation();
+//	fourthSceneAnimation();
+//	fiveSceneAnimation();
+//	sevenSceneAnimation();
+//	eightSceneAnimation();
+//	nineSceneAnimation();
+//	tenAnimation();
+//	elevenAnimation();
+//	twelfthAnimation();
+//	fourteenthSceneAnimation();
+//	sixteenthSceneAnimation();
+//	seventeenthSceneAnimation();
+//	eighteenthSceneAnimation();
+//	sixSceneAnimation();
+//	activePopupMenuLink();
+//	activeStaticDesktopMenuLink();
+});
